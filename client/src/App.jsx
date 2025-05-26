72946ebd9d08b67f115a40ae4a7ace0a347b518e
Bhaveshanjana <bhaveshanjana58@gmail.com>
2025-05-26 23:47:15 +0530

    Working on home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
     <div>
-      home
+      <div>
+        <NavBar />
+        <Hero />
+        <h1 className="text-white text-xl mx-7">Silhouette No. 1 – Vermilion</h1>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
